8cea7fd3760827e1e33a51cd18878348bf217d3a
Hayden Schroeder <hschroeder0317@gmail.com>
2022-12-28 22:00:43 -0600

    Animations work. Need to add button for generating new rule :fuelpump:

--- python/mergematrix.py
+++ python/mergematrix.py
@@ -1,5 +1,12 @@
 from secrets import token_hex
+from viewer import viewer
+
+HEIGHT = 15
+WIDTH = 20
+INTERVAL = 100
 
 def gen_rule():
     return token_hex(16)
 
+myviewer = viewer(HEIGHT, WIDTH, gen_rule(), INTERVAL)
+myviewer.go_animate()
