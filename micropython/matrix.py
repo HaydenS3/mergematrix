75ef2f6a833d696c041e02001f207059540e6cb8
Hayden Schroeder <hschroeder0317@gmail.com>
2023-01-03 08:00:35 -0600

    Working on matrix print :dolphin:

--- micropython/matrix.py
+++ micropython/matrix.py
@@ -1,46 +1,18 @@
+# Colors to be used for printing the matrix
 class colors:
-    reset = '\033[0m'
-    bold = '\033[01m'
-    disable = '\033[02m'
-    underline = '\033[04m'
-    reverse = '\033[07m'
-    strikethrough = '\033[09m'
-    invisible = '\033[08m'
-    
-    class fg:
-            black = '\033[30m'
-            red = '\033[31m'
-            green = '\033[32m'
-            orange = '\033[33m'
-            blue = '\033[34m'
-            purple = '\033[35m'
-            cyan = '\033[36m'
-            lightgrey = '\033[37m'
-            darkgrey = '\033[90m'
-            lightred = '\033[91m'
-            lightgreen = '\033[92m'
-            yellow = '\033[93m'
-            lightblue = '\033[94m'
-            pink = '\033[95m'
-            lightcyan = '\033[96m'
-    
-    class bg:
-        black = '\033[40m'
-        red = '\033[41m'
-        green = '\033[42m'
-        orange = '\033[43m'
-        blue = '\033[44m'
-        purple = '\033[45m'
-        cyan = '\033[46m'
-        lightgrey = '\033[47m'
+    black = '\033[40m'
+    red = '\033[41m'
+    green = '\033[42m'
+    yellow = '\033[43m'
+    blue = '\033[44m'
+    purple = '\033[45m'
+    cyan = '\033[46m'
+    white = '\033[47m'
 
 class matrix:
+    # Prints a color to the terminal
     def printcolor(color):
         print(color, end=' ')
- 
-# print(colors.bg.green, "SKk", colors.fg.red, "Amartya")
-# print(colors.bg.lightgrey, "SKk", colors.fg.red, "Amartya")
-matrix.printcolor(colors.bg.red)
-matrix.printcolor(colors.bg.green)
-matrix.printcolor(colors.bg.orange)
-matrix.printcolor(colors.bg.blue)
+    # Prints a matrix to the terminal
+    def printmatrix(matrix):
+        print("do something")
